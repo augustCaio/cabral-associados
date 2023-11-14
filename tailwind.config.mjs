30c9f9cabb8750cf0da88f0853b01c9b05ce98d3
augustCaio <caioaugusto930@gmail.com>
2023-11-13 21:49:19 -0300

    demo site cabral associados

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        whatsapp_green_primary: "#25D366",
+        whatsapp_green_secondary: "#128C7E",
+        backdrop: "#730707",
+        primary_button: "#084DA6",
+        background: "#022859",
+      },
+      backgroundImage: {
+        "hero-bg": "url('/background-hero.jpg')",
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
